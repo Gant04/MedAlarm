6c02ba582bb1db19ecf7157e0e3dfd4bacd8f05c
Mike Adams <magnavoid@gmail.com>
2015-06-13 17:47:49 -0600

    Updated Alarm Class.

--- app/src/main/java/concentric/medalarm/models/Alarm.java
+++ app/src/main/java/concentric/medalarm/models/Alarm.java
@@ -6,12 +6,71 @@ package concentric.medalarm.models;
 public class Alarm {
 
     public static final String TABLE_NAME = "alarm";
-    public static final String COLUMN_NAME_ALARM_NAME = "";
-    public static final String COLUMN_NAME_ALARM_TIME_HOUR = "";
-    public static final String COLUMN_NAME_ALARM_TIME_MINUTE = "";
-    public static final String COLUMN_NAME_ALARM_REPEAT_DAYS = "";
-    public static final String COLUMN_NAME_ALARM_REPEAT_WEEKLY = "";
-    public static final String COLUMN_NAME_ALARM_TONE = "";
-    public static final String COLUMN_NAME_ALARM_ENABLED = "";
+    public static String COLUMN_NAME_ALARM_NAME = "";
+    public static String COLUMN_NAME_ALARM_TIME_HOUR = "0";
+    public static String COLUMN_NAME_ALARM_TIME_MINUTE = "0";
+    public static String COLUMN_NAME_ALARM_REPEAT_DAYS = "0";
+    public static String COLUMN_NAME_ALARM_REPEAT_WEEKLY = "0";
+    public static String COLUMN_NAME_ALARM_TONE = "";
+    public static String COLUMN_NAME_ALARM_ENABLED = "";
 
+    public static String getTableName() {
+        return TABLE_NAME;
+    }
+
+    public static String getColumnNameAlarmName() {
+        return COLUMN_NAME_ALARM_NAME;
+    }
+
+    public static void setColumnNameAlarmName(String columnNameAlarmName) {
+        COLUMN_NAME_ALARM_NAME = columnNameAlarmName;
+    }
+
+    public static String getColumnNameAlarmTimeHour() {
+        return COLUMN_NAME_ALARM_TIME_HOUR;
+    }
+
+    public static void setColumnNameAlarmTimeHour(String columnNameAlarmTimeHour) {
+        COLUMN_NAME_ALARM_TIME_HOUR = columnNameAlarmTimeHour;
+    }
+
+    public static String getColumnNameAlarmTimeMinute() {
+        return COLUMN_NAME_ALARM_TIME_MINUTE;
+    }
+
+    public static void setColumnNameAlarmTimeMinute(String columnNameAlarmTimeMinute) {
+        COLUMN_NAME_ALARM_TIME_MINUTE = columnNameAlarmTimeMinute;
+    }
+
+    public static String getColumnNameAlarmRepeatDays() {
+        return COLUMN_NAME_ALARM_REPEAT_DAYS;
+    }
+
+    public static void setColumnNameAlarmRepeatDays(String columnNameAlarmRepeatDays) {
+        COLUMN_NAME_ALARM_REPEAT_DAYS = columnNameAlarmRepeatDays;
+    }
+
+    public static String getColumnNameAlarmRepeatWeekly() {
+        return COLUMN_NAME_ALARM_REPEAT_WEEKLY;
+    }
+
+    public static void setColumnNameAlarmRepeatWeekly(String columnNameAlarmRepeatWeekly) {
+        COLUMN_NAME_ALARM_REPEAT_WEEKLY = columnNameAlarmRepeatWeekly;
+    }
+
+    public static String getColumnNameAlarmTone() {
+        return COLUMN_NAME_ALARM_TONE;
+    }
+
+    public static void setColumnNameAlarmTone(String columnNameAlarmTone) {
+        COLUMN_NAME_ALARM_TONE = columnNameAlarmTone;
+    }
+
+    public static String getColumnNameAlarmEnabled() {
+        return COLUMN_NAME_ALARM_ENABLED;
+    }
+
+    public static void setColumnNameAlarmEnabled(String columnNameAlarmEnabled) {
+        COLUMN_NAME_ALARM_ENABLED = columnNameAlarmEnabled;
+    }
 }
