bd1d196fe35250d73e118f31dfbe7601e1365caf
LuckyGinger <LuckyGing3r@gmail.com>
2015-06-13 19:31:06 -0600

    implemented AlarmTest

--- app/src/main/java/concentric/medalarm/AlarmTest.java
+++ app/src/main/java/concentric/medalarm/AlarmTest.java
@@ -13,6 +13,16 @@ public class AlarmTest {
 
     public void runTest(){
         Alarm alarm = new Alarm(hour, minute, daysToRepeat, repeatWeekly);
+        // Tests
+        assert( hour >= 0 || hour < 24 );
+        assert( minute >= 0 || minute < 60);
 
+        assert(alarm.TABLE_NAME == "alarm");
+        assert(alarm.COLUMN_NAME_ALARM_GROUP == "group");
+        assert(alarm.COLUMN_NAME_ALARM_ID == "id");
+        assert(alarm.COLUMN_NAME_ALARM_TIME_HOUR == "hour");
+        assert(alarm.COLUMN_NAME_ALARM_TIME_MINUTE == "minute");
+        assert(alarm.COLUMN_NAME_ALARM_REPEATS_DAYS == "repeatDays");
+        assert(alarm.COLUMN_NAME_ALARM_REPEATS_WEEKLY == "repeatWeeks");
     }
 }
