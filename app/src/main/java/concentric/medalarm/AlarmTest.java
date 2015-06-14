e0bd4b9878d1e09bd83f740e7661a463ae871f38
LuckyGinger <LuckyGing3r@gmail.com>
2015-06-13 21:24:08 -0600

    fixed AlarmTest asserts

--- app/src/main/java/concentric/medalarm/AlarmTest.java
+++ app/src/main/java/concentric/medalarm/AlarmTest.java
@@ -7,14 +7,16 @@ import concentric.medalarm.models.Alarm;
  */
 public class AlarmTest {
     int hour = 24;
-    int minute = 30;
+    int minute = 90;
     boolean daysToRepeat[] = new boolean[] {false, true, false, true, false, true, false};
     boolean repeatWeekly = false;
 
     public void runTest(){
         Alarm alarm = new Alarm(hour, minute, daysToRepeat, repeatWeekly);
-        assert(alarm.getHour() >= 0 && alarm.getHour() < 24);
-        assert(alarm.getMinute() >= 0 && alarm.getMinute() < 60);
+        assert(alarm.getHour() >= 0);
+        assert(alarm.getHour() < 24);
+        assert(alarm.getMinute() >= 0);
+        assert(alarm.getMinute() < 60);
         assert(alarm.TABLE_NAME == "alarm");
         assert(alarm.COLUMN_NAME_ALARM_GROUP == "group");
         assert(alarm.COLUMN_NAME_ALARM_ID == "id");
