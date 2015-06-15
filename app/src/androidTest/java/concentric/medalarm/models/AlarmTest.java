27a465d1be66ae33bab1b144375fe0b437cee24a
LuckyGinger <LuckyGing3r@gmail.com>
2015-06-15 12:09:52 -0600

    fixed AlarmTest asserts

--- app/src/androidTest/java/concentric/medalarm/models/AlarmTest.java
+++ app/src/androidTest/java/concentric/medalarm/models/AlarmTest.java
@@ -1,28 +1,32 @@
 package concentric.medalarm.models;
 
 import junit.framework.TestCase;
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
 
 /**
  * Created by MatthewAry on 6/13/2015.
  */
-public class AlarmTest extends TestCase {
+public class AlarmTest extends AndroidTestCase {
 
-    int hour = 23;
-    int minute = 90;
-    boolean daysToRepeat[] = new boolean[]{false, true, false, true, false, true, false};
-    boolean repeatWeekly = false;
+    public int hour = 23;
+    public int minute = 50;
+    public boolean daysToRepeat[] = new boolean[]{false, true, false, true, false, true, false};
+    public boolean repeatWeekly = false;
 
-    public void runTest() {
+    public void testAlarm() throws Throwable{
         Alarm alarm = new Alarm(hour, minute, daysToRepeat, repeatWeekly);
-        assert (alarm.getHour() >= 0 && alarm.getHour() < 23);
-        assert (alarm.getMinute() >= 0 && alarm.getMinute() < 60);
-        assert (!alarm.isRepeatWeekly());
-        assert (Alarm.TABLE_NAME.equals("alarm"));
-        assert (Alarm.COLUMN_NAME_ALARM_GROUP.equals("group"));
-        assert (Alarm.COLUMN_NAME_ALARM_ID.equals("id"));
-        assert (Alarm.COLUMN_NAME_ALARM_TIME_HOUR.equals("hour"));
-        assert (Alarm.COLUMN_NAME_ALARM_TIME_MINUTE.equals("minute"));
-        assert (Alarm.COLUMN_NAME_ALARM_REPEATS_DAYS.equals("days"));
-        assert (Alarm.COLUMN_NAME_ALARM_REPEATS_WEEKLY.equals("repeatWeeks"));
+        Assert.assertTrue (alarm.getHour() >= 0);
+        Assert.assertTrue(alarm.getHour() < 24);
+        Assert.assertTrue(alarm.getMinute() >= 0);
+        Assert.assertTrue(alarm.getMinute() < 60);
+        Assert.assertTrue(!alarm.isRepeatWeekly());
+        Assert.assertTrue(Alarm.TABLE_NAME.equals("alarm"));
+        Assert.assertTrue(Alarm.COLUMN_NAME_ALARM_GROUP.equals("group"));
+        Assert.assertTrue(Alarm.COLUMN_NAME_ALARM_ID.equals("id"));
+        Assert.assertTrue(Alarm.COLUMN_NAME_ALARM_TIME_HOUR.equals("hour"));
+        Assert.assertTrue(Alarm.COLUMN_NAME_ALARM_TIME_MINUTE.equals("minute"));
+        Assert.assertTrue(Alarm.COLUMN_NAME_ALARM_REPEATS_DAYS.equals("repeatDays"));
+        Assert.assertTrue(Alarm.COLUMN_NAME_ALARM_REPEATS_WEEKLY.equals("repeatWeeks"));
     }
 }
