e5787f1bb6b6365b483c2f7c50650e615d4385b3
Mike Adams <magnavoid@gmail.com>
2015-06-20 18:36:44 -0600

    Stubbed out a ton of methods, and classes. Additonal work was done including the start of the eventFactory and many related classes and methods.

--- app/src/main/java/concentric/medalarm/AlarmPreferences.java
+++ app/src/main/java/concentric/medalarm/AlarmPreferences.java
@@ -4,4 +4,22 @@ package concentric.medalarm;
  * Created by Thom on 6/10/2015.
  */
 public class AlarmPreferences {
+    private boolean isRepeatable;
+    private boolean[] daysToRepeat = new boolean[7];
+
+    public boolean isRepeatable() {
+        return isRepeatable;
+    }
+
+    public void setIsRepeatable(boolean isRepeatable) {
+        this.isRepeatable = isRepeatable;
+    }
+
+    public boolean[] getDaysToRepeat() {
+        return daysToRepeat;
+    }
+
+    public void setDaysToRepeat(boolean[] daysToRepeat) {
+        this.daysToRepeat = daysToRepeat;
+    }
 }
