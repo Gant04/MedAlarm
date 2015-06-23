4d00e0bbb8f5d82f596347c6b5a7fd566b5e4b28
Mike Adams <magnavoid@gmail.com>
2015-06-23 16:58:58 -0600

    Broke Main Activity and made TimeConverter Class

--- app/src/main/java/concentric/medalarm/TimeConverter.java
+++ app/src/main/java/concentric/medalarm/TimeConverter.java
@@ -4,4 +4,25 @@ package concentric.medalarm;
  * Created by mike on 6/23/15.
  */
 public class TimeConverter {
+    public static long minutesToMillis(int minute) {
+        long millis = 0;
+
+
+        return millis;
+    }
+
+    public static long hoursToMillis(int minute) {
+        long millis = 0;
+
+
+        return millis;
+    }
+
+    public static long secondsToMillis(int minute) {
+        long millis = 0;
+
+
+        return millis;
+    }
+
 }
