fd783190bfe055c97c355f3d02aee348e1227899
LuckyGinger <LuckyGing3r@gmail.com>
2015-06-23 17:42:07 -0600

    Edited TimeConverter the Milliseconds

--- app/src/main/java/concentric/medalarm/TimeConverter.java
+++ app/src/main/java/concentric/medalarm/TimeConverter.java
@@ -4,17 +4,13 @@ package concentric.medalarm;
  * Created by mike on 6/23/15.
  */
 public class TimeConverter {
+    private static long minMilli = 60000;   // 60,000 milliseconds in a minute
+
     public static long minutesToMillis(int minute) {
-        long millis = 0;
-
-
-        return millis;
+        return (minute * minMilli);
     }
 
-    public static long hoursToMillis(int minute) {
-        long millis = 0;
-
-
-        return millis;
+    public static long hoursToMillis(int hours) {
+        return (hours * 60 * minMilli);
     }
 }
