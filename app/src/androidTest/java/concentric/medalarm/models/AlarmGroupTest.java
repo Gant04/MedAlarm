6b918379ee8745a9e8770a0af5e18b99ced73518
Matthew Ary <matthewj.c.ary@gmail.com>
2015-06-24 16:37:33 -0600

    Adding more tests and logs to the system.

--- app/src/androidTest/java/concentric/medalarm/models/AlarmGroupTest.java
+++ app/src/androidTest/java/concentric/medalarm/models/AlarmGroupTest.java
@@ -8,10 +8,26 @@ import android.test.AndroidTestCase;
 public class AlarmGroupTest extends AndroidTestCase {
     public void testAlarmGroup() throws Throwable {
         AlarmGroup alarmGroup = new AlarmGroup();
-            Assert.assertTrue(alarmGroup.setAlarmType("Day") == true);
-            Assert.assertTrue(alarmGroup.setAlarmType("Week") == true);
-            Assert.assertTrue(alarmGroup.setAlarmType("Interval") == true);
-            Assert.assertTrue(alarmGroup.setAlarmType("Once") == true);
-            Assert.assertTrue(alarmGroup.setAlarmType("FAIL") == false);
+        Assert.assertTrue(alarmGroup.setAlarmType("Day") == true);
+        Assert.assertTrue(alarmGroup.setAlarmType("Week") == true);
+        Assert.assertTrue(alarmGroup.setAlarmType("Interval") == true);
+        Assert.assertTrue(alarmGroup.setAlarmType("Once") == true);
+        Assert.assertTrue(alarmGroup.setAlarmType("FAIL") == false);
+        alarmGroup.setEnabled(true);
+        Assert.assertTrue(alarmGroup.getEnabled() == true);
+        alarmGroup.setEnabled(true);
+        Assert.assertTrue(alarmGroup.getEnabled() == true);
+        alarmGroup.setEnabled(false);
+        Assert.assertTrue(alarmGroup.getEnabled() == false);
+        alarmGroup.setEnabled(false);
+        Assert.assertFalse(alarmGroup.getEnabled() == true);
+        alarmGroup.setVibrate(true);
+        Assert.assertTrue(alarmGroup.getVibrate() == true);
+        alarmGroup.setVibrate(true);
+        Assert.assertTrue(alarmGroup.getVibrate() == true);
+        alarmGroup.setVibrate(false);
+        Assert.assertTrue(alarmGroup.getVibrate() == false);
+        alarmGroup.setVibrate(false);
+        Assert.assertFalse(alarmGroup.getVibrate() == true);
     }
 }
