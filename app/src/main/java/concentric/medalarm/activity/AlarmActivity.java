3f6f6442991c0e74019168dd3c1d883ca0f08a5d
LuckyGinger <LuckyGing3r@gmail.com>
2015-06-25 19:09:01 -0600

    Working on the Alarm prototype Added AlarmActivity.java Added Other stuff

---
+++ app/src/main/java/concentric/medalarm/activity/AlarmActivity.java
@@ -0,0 +1,92 @@
+package concentric.medalarm.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
+
+import concentric.medalarm.AlarmBroadcastReceiver;
+import concentric.medalarm.R;
+
+public class AlarmActivity extends Activity {
+
+    AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+    private TimePicker alarmTimePicker;
+    private static AlarmActivity inst;
+    private TextView alarmTextView;
+
+    public static AlarmActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        alarmTimePicker = (TimePicker) findViewById(R.id.time_picker);
+        alarmTextView = (TextView) findViewById(R.id.text);
+        ToggleButton alarmToggle = (ToggleButton) findViewById(R.id.alarmToggle);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+    }
+
+    public void onToggleClicked(View view) {
+        if (((ToggleButton) view).isChecked()) {
+            Log.d("MyActivity", "Alarm On");
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+            Intent myIntent = new Intent(AlarmActivity.this, AlarmBroadcastReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this, 0, myIntent, 0);
+            alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
+        } else {
+            alarmManager.cancel(pendingIntent);
+            setAlarmText("");
+            Log.d("MyActivity", "Alarm Off");
+        }
+    }
+
+    public void setAlarmText(String alarmText) {
+        alarmTextView.setText(alarmText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_alarm, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/concentric/medalarm/activity/MainActivity.java
+++ app/src/main/java/concentric/medalarm/activity/MainActivity.java
@@ -1,10 +1,13 @@
 package concentric.medalarm.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
@@ -16,6 +19,7 @@ import concentric.medalarm.R;
 
 
 public class MainActivity extends AppCompatActivity {
+    Button createButton = (Button)findViewById(R.id.button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +51,13 @@ public class MainActivity extends AppCompatActivity {
         result.closeDrawer();
         result.isDrawerOpen();
 
-
-
+        createButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AlarmActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     @Override
@@ -72,4 +81,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
