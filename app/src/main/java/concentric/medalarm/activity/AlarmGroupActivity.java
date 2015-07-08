5b393f68e5bb9cacc3f93e1b24d482a87e690551
Matthew Ary <matthewj.c.ary@gmail.com>
2015-07-07 20:40:21 -0600

    Did work on the Alarm Group activity and starting work on detecting changes to the spinner used to define the alarm type.

--- app/src/main/java/concentric/medalarm/activity/AlarmGroupActivity.java
+++ app/src/main/java/concentric/medalarm/activity/AlarmGroupActivity.java
@@ -1,18 +1,39 @@
 package concentric.medalarm.activity;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import concentric.medalarm.R;
 
-public class AlarmGroupActivity extends ActionBarActivity {
+public class AlarmGroupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_group);
+
+        // Get Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.AlarmGroupToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Populate Spinner
+        AppCompatSpinner spinner = (AppCompatSpinner) findViewById(R.id.AlarmType);
+        spinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array
+                .alarm_types, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     @Override
@@ -35,5 +56,58 @@ public class AlarmGroupActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * <p>Callback method to be invoked when an item in this view has been
+     * selected. This callback is invoked only when the newly selected
+     * position is different from the previously selected position or if
+     * there was no selected item.</p>
+     *
+     * Impelmenters can call getItemAtPosition(position) if they need to access the
+     * data associated with the selected item.
+     *
+     * <p>Used in this case for when an item in the spinners have been selected</p>
+     * @param parent The AdapterView where the selection happened
+     * @param view The view within the AdapterView that was clicked
+     * @param pos The position of the view in the adapter
+     * @param id The row id of the item that is selected
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        String[] types = getResources().getStringArray(R.array.alarm_types);
+        if (pos > 0) {
+            // Test which picker has been toggled.
+            if (types[pos].equals(parent.getItemAtPosition(pos))) {
+                Log.i(getClass().getName() + " onItemSelected", types[pos] + " was " +
+                        "selected.");
+                switch (pos) {
+                    case 1:
+                        // TODO: Handle the selection of Daily
+                        break;
+                    case 2:
+                        // TODO: Handle the selection of Weekly
+                        break;
+                    case 3:
+                        // TODO: Handle the selection of Intervals
+                        break;
+                    case 4:
+                        // TODO: Handle the selection of Only Once
+                        break;
+
+                }
+            }
+        }
+    }
+
+    /**
+     * Callback method to be invoked when the selection disappears from this
+     * view. The selection can disappear for instance when touch is activated
+     * or when the adapter becomes empty.
+     *
+     * @param parent The AdapterView that now contains no selected item.
+     */
+    public void onNothingSelected(AdapterView<?> parent) {
+        // required interface callback.
     }
 }
