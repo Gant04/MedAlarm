9de5dbcab017847174c8a60f3f93d9e8926b8e21
Matthew Ary <matthewj.c.ary@gmail.com>
2015-07-09 16:05:09 -0600

    Working on Cardviews THIS IS BROKEN!!!!

--- app/src/main/java/concentric/medalarm/AlarmGroupCardAdapter.java
+++ app/src/main/java/concentric/medalarm/AlarmGroupCardAdapter.java
@@ -6,54 +6,61 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
+import concentric.medalarm.models.AlarmGroup;
+
 /**
  * Created by MatthewAry on 7/9/2015.
  */
 public class AlarmGroupCardAdapter extends RecyclerView.Adapter<AlarmGroupCardAdapter.ViewHolder> {
-    private String[] dataSet;
-
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView textView;
-        public ViewHolder(TextView v) {
-            super(v);
-            textView = v;
-        }
-    }
+    private List<AlarmGroup> alarmGroups;
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public AlarmGroupCardAdapter(String[] dataSet) {
-        this.dataSet = dataSet;
-    }
-
-    // Create new views (invoked by the layout manager)
-    @Override
-    public AlarmGroupCardAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // create a new view
-/*        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.my_text_view, parent, false);*/
-        // set the view's size, margins, paddings and layout parameters
-
-        /*ViewHolder vh = new ViewHolder(v);
-        return vh;*/
-        return null;
-    }
-
-    // Replace the contents of a view (invoked by the layout manager)
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.textView.setText(dataSet[position]);
-
+    public AlarmGroupCardAdapter(List<AlarmGroup> dataSet) {
+        alarmGroups = dataSet;
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return dataSet.length;
+        return alarmGroups.size();
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        AlarmGroup item = alarmGroups.get(position);
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        ViewHolder.groupName.setText();
+
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.alarm_group_card, parent, false);
+        // Optionally set the view's size, margins, paddings and layout parameters below
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        protected TextView groupName;
+        protected TextView groupType;
+
+        public ViewHolder(View v) {
+            super(v);
+            groupName = (TextView) v.findViewById(R.id.groupName);
+            groupType = (TextView) v.findViewById(R.id.groupType);
+        }
     }
 }
