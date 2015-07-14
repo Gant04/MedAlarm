47e93eb9e902b0016b8e0d76cc4735ab94a23d16
Michael Adams <magnavoid@gmail.com>
2015-07-14 16:21:18 -0600

    AlarmGroupManager work almost complete.

--- app/src/main/java/concentric/medalarm/MedAlarmManager.java
+++ app/src/main/java/concentric/medalarm/MedAlarmManager.java
@@ -1,11 +1,15 @@
 package concentric.medalarm;
 
+import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.Time;
 import android.util.Log;
 
+import java.util.Calendar;
 import java.util.List;
 
 import concentric.medalarm.models.Alarm;
@@ -59,6 +63,10 @@ public class MedAlarmManager extends AppCompatActivity {
 
                 alarmBundle.putString("med", medicationName);    //add Medication Name
 
+                intent.putExtras(alarmBundle);
+
+                createAlarm(intent);
+
             }
         }
     }
@@ -91,5 +99,64 @@ public class MedAlarmManager extends AppCompatActivity {
             }
 
         }
+    }
+
+    private void createAlarm(final Intent alarmIntent) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+
+        int hour, minute, second, milli;
+        hour = minute = second = milli = 0;
+
+        long id, groupID;
+        id = groupID = 0;
+
+        String medicationName = "";
+
+        alarmIntent.getIntExtra("hour", hour);                //add Hours
+        alarmIntent.getIntExtra("minute", minute);            //add Minutes
+        alarmIntent.getIntExtra("second", second);           //add Seconds
+        alarmIntent.getIntExtra("milli", milli);            //add Millis
+
+        alarmIntent.getLongExtra("id", id);                  //add Alarm ID
+        alarmIntent.getLongExtra("groupID", groupID);        //add Group ID
+
+        //alarmIntent.getStringExtra("med", medicationName);    //add Medication Name
+
+        Long longHours = TimeConverter.hoursToMillis(hour);
+        Long longMinutes = TimeConverter.minutesToMillis(minute);
+
+        Long longTime = longHours + longMinutes;
+
+        //Calculate difference between current time and future time AKA difference
+        // TODO: Time is depreciated. Change to something else.
+        Time time = new Time();
+        time.setToNow();
+
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, second);
+        calendar.set(Calendar.MILLISECOND, milli);
+
+        final long difference = calendar.getTimeInMillis() - time.toMillis(true);
+        Log.d("Time Difference:    ", Long.toString(difference));
+
+
+        if (calendar.getTimeInMillis() < time.toMillis(true)) {
+            calendar.set(Calendar.DAY_OF_YEAR, Calendar.DAY_OF_YEAR + 1);
+        }
+
+
+        /*
+        new Intent("com.concentric.alarmIntent." + med)
+        this allows to generate new intents on the fly.
+        */
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+        AlarmManager alarmManager = (AlarmManager) (this.getSystemService(Context.ALARM_SERVICE));
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+
+
     }
 }
