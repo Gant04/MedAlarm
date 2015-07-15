d3ec20072e36262416e9f73f6d349d11d687cce0
Matthew Ary <matthewj.c.ary@gmail.com>
2015-07-14 19:21:13 -0600

    Some minor work.

--- app/src/main/java/concentric/medalarm/AlarmGroupCardAdapter.java
+++ app/src/main/java/concentric/medalarm/AlarmGroupCardAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.List;
@@ -66,6 +67,7 @@ public class AlarmGroupCardAdapter extends RecyclerView.Adapter<AlarmGroupCardAd
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         protected static TextView groupName;
         protected static TextView groupType;
+        protected static TableRow itemDetails;
         protected alarmGroupViewHolderClicks mListener;
 
         public ViewHolder(View v) {
