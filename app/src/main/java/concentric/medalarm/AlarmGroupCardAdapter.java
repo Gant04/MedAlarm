d459571ed01df790991eb602dd3c7d724a6213a9
Matthew Ary <matthewj.c.ary@gmail.com>
2015-07-15 17:32:19 -0600

    Working on animation. Getting somewhere... Kinda

--- app/src/main/java/concentric/medalarm/AlarmGroupCardAdapter.java
+++ app/src/main/java/concentric/medalarm/AlarmGroupCardAdapter.java
@@ -1,9 +1,18 @@
 package concentric.medalarm;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.support.design.widget.TabLayout;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageButton;
 import android.widget.TableRow;
 import android.widget.TextView;
 
@@ -48,14 +57,63 @@ public class AlarmGroupCardAdapter extends RecyclerView.Adapter<AlarmGroupCardAd
     @Override
     public AlarmGroupCardAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
-        View v = LayoutInflater.from(parent.getContext())
+        final View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.alarm_group_card, parent, false);
         // Optionally set the view's size, margins, paddings and layout parameters below
 
         AlarmGroupCardAdapter.ViewHolder vh = new ViewHolder(v, new AlarmGroupCardAdapter
-                .ViewHolder.ViewHolderClicks() {
-            public void toggleControls(View caller) {
+                .ViewHolderClicks() {
+            private boolean rotated = false;
+            private View line = v.findViewById(R.id.line);
+            private View controls = v.findViewById(R.id.controls);
+            private View arrow = v.findViewById(R.id.expandCollapse);
 
+            /**
+             * Called when the table Row is clicked.
+             *
+             * See: http://goo.gl/gR5aZH
+             * See:
+             * for details on how this works.
+             * @param caller the View of item that was touched.
+             */
+            public void toggleControls(View caller) {
+                ObjectAnimator cheveron = null;
+                ObjectAnimator aniControls = null;
+                ObjectAnimator aniLine = null;
+                float lineHight = (float) line.getHeight();
+                float controlHeight = (float) controls.getHeight();
+
+                if (!rotated) {
+                    cheveron = ObjectAnimator.ofFloat(arrow, "rotation", 0.0f, 180f);
+                    aniControls = ObjectAnimator.ofFloat(controls, "translationY", controlHeight, 0.0f);
+                    aniLine = ObjectAnimator.ofFloat(line, "translationY", lineHight, 0.0f);
+                    rotated = true;
+                } else {
+                    cheveron = ObjectAnimator.ofFloat(arrow, "rotation", 180f, 0.0f);
+                    aniControls = ObjectAnimator.ofFloat(controls, "translationY", 0.0f, controlHeight);
+                    aniLine = ObjectAnimator.ofFloat(line, "translationY", 0.0f, lineHight);
+                    rotated = false;
+                }
+
+                aniControls.setDuration(300);
+                aniControls.setInterpolator(new AccelerateDecelerateInterpolator());
+                aniLine.setDuration(100);
+                aniLine.setInterpolator(new LinearInterpolator());
+                cheveron.setDuration(200);
+                cheveron.setInterpolator(new AccelerateDecelerateInterpolator());
+
+                ImageButton button = (ImageButton) caller.findViewById(R.id.expandCollapse);
+
+                AnimatorSet s = new AnimatorSet();
+                cheveron.setRepeatCount(0);
+                aniControls.setRepeatCount(0);
+                aniLine.setRepeatCount(0);
+
+                s.play(cheveron).with(aniLine).with(aniControls);
+                s.start();
+
+
+                Log.d(getClass().getName() + " toggleControls", "Should have animated.");
             }
         });
         return vh;
@@ -87,9 +145,8 @@ public class AlarmGroupCardAdapter extends RecyclerView.Adapter<AlarmGroupCardAd
                 mListener.toggleControls(v);
             }
         }
-
-        public interface ViewHolderClicks {
-            void toggleControls(View caller);
-        }
+    }
+    public interface ViewHolderClicks {
+        void toggleControls(View caller);
     }
 }
