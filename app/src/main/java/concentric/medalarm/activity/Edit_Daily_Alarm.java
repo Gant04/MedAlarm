6d33232a1f2d65e262d8b0591d96e73861282375
Matthew Ary <matthewj.c.ary@gmail.com>
2015-07-18 17:37:02 -0600

    Started getting work done on the more complex functions of the edit activyt.

--- app/src/main/java/concentric/medalarm/activity/Edit_Daily_Alarm.java
+++ app/src/main/java/concentric/medalarm/activity/Edit_Daily_Alarm.java
@@ -1,19 +1,42 @@
 package concentric.medalarm.activity;
 
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TimePicker;
+
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.List;
+
+import concentric.medalarm.CustomListViewAdapter;
 import concentric.medalarm.R;
+import concentric.medalarm.models.Alarm;
+import concentric.medalarm.models.AlarmDataSource;
 import concentric.medalarm.models.AlarmGroup;
 import concentric.medalarm.models.AlarmGroupDataSource;
 
 public class Edit_Daily_Alarm extends AppCompatActivity {
-    AlarmGroup alarmGroup;
+    private AlarmGroup alarmGroup;
+    private List<Alarm> alarms;
+    private ListView listView;
+    private CustomListViewAdapter adapter;
+    private Calendar dateAndTime = Calendar.getInstance();
+
+    private TimePickerDialog.OnTimeSetListener tp = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay,
+                              int minute) {
+            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateAndTime.set(Calendar.MINUTE, minute);
+           // updateList();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +49,18 @@ public class Edit_Daily_Alarm extends AppCompatActivity {
 
         // Load in alarm details
         AlarmGroupDataSource db = new AlarmGroupDataSource();
+        AlarmDataSource dba = new AlarmDataSource();
         try {
             db.open();
+            Bundle extras = getIntent().getExtras();
+            alarmGroup = db.getAlarmGroup(extras.getLong("groupID"));
+            db.close();
+            dba.open();
+            alarms = dba.getGroupAlarms(alarmGroup.getId());
+            dba.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        Bundle extras = getIntent().getExtras();
-        alarmGroup = db.getAlarmGroup(extras.getLong("groupID"));
-        db.close();
 
         // Set alarm details
         EditText name = (EditText) findViewById(R.id.alarmName);
