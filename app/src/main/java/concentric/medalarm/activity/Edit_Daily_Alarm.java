0c4fa02e51deb1cbf030875efb49c93cbb4b7d8d
Matthew Ary <matthewj.c.ary@gmail.com>
2015-07-18 23:46:47 -0600

    2

--- app/src/main/java/concentric/medalarm/activity/Edit_Daily_Alarm.java
+++ app/src/main/java/concentric/medalarm/activity/Edit_Daily_Alarm.java
@@ -140,17 +140,21 @@ public class Edit_Daily_Alarm extends AppCompatActivity {
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.save:
+                TextView name = (TextView) findViewById(R.id.alarmName);
+                if (!name.getText().toString().isEmpty()){
+                    save();
+                    setResult(RESULT_OK);
+                    finish();// TODO: Confirm Save
+                }
+                else {
+                    Toast.makeText(this,"Medication name cannot be empty.",Toast.LENGTH_LONG).show();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
