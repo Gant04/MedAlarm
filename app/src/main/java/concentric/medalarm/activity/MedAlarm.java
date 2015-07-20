b8c39e0cccd50ef08f0edf279ad6682609107e5e
Michael Adams <magnavoid@gmail.com>
2015-07-19 21:40:50 -0600

    Cleaned code.

--- app/src/main/java/concentric/medalarm/activity/MedAlarm.java
+++ app/src/main/java/concentric/medalarm/activity/MedAlarm.java
@@ -54,16 +54,6 @@ public class MedAlarm extends Activity {
     Boolean dbLoaded = false;
     Handler mHideHandler = new Handler();
     /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
-        }
-    };
-    /**
      * Touch listener to use for in-layout UI controls to delay hiding the
      * system UI. This is to prevent the jarring behavior of controls going away
      * while interacting with activity UI.
@@ -75,6 +65,16 @@ public class MedAlarm extends Activity {
                 delayedHide(AUTO_HIDE_DELAY_MILLIS);
             }
             return false;
+        }
+    };
+    /**
+     * The instance of the {@link SystemUiHider} for this activity.
+     */
+    private SystemUiHider mSystemUiHider;
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mSystemUiHider.hide();
         }
     };
 
@@ -123,6 +123,9 @@ public class MedAlarm extends Activity {
         bootReceiver.start();
     }
 
+    /**
+     * This private method initalizes and starts up the database through a thread.
+     */
     private void regesterDBhelper() {
         Thread dbHelper = new Thread() {
             @Override
